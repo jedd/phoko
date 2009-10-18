e4ae7233ac59693f1fd20582125292d3ca3870fe
jedd <unclejedd@gmail.com>
2009-10-18 23:48:44 +0100

    Tweak - pro forma headings for gallery controller

--- app/controllers/gallery.php
+++ app/controllers/gallery.php
@@ -1,17 +1,64 @@
 <?php
+/**
+ * Phoko
+ *
+ * Web gallery for a KPhotoAlbum repository
+ *
+ * @package		phoko
+ * @author		jedd
+ * @version		v1
+ * @copyright	Copyright (c) 2009, jedd
+ * @license		GPL 2 or later
+ * @link		http://dingogully.com.au/trac/phoko
+ **/
 
-class Gallery extends Controller {
+// ------------------------------------------------------------------------
 
-	function Gallery()
-	{
-		parent::Controller();	
-	}
+/**
+ * Gallery
+ *
+ * Primary controller for the Phoko suite.
+ *
+ * @package     phoko
+ * @version     v1
+ * @author      jedd
+ * @link        http://dingogully.com.au/trac/phoko
+ **/
+
+// ------------------------------------------------------------------------
+
+class  Gallery extends  Controller {
+
+	// ------------------------------------------------------------------------
+	/**
+	 *   Attributes
+	 **/
+
+
+	// ------------------------------------------------------------------------
+	/**
+	 *   Constructor
+	 **/
+
+	function  __construct ()  {
+		parent::Controller();
+		} // end-constructor
+
+
+
+	// ------------------------------------------------------------------------
+	/**
+	 *   index
+	 **/
 	
 	function index()
 	{
 		$this->load->view('welcome_message');
 	}
-}
+
+
+
+	}   // end-class  gallery ()
 
 /* End of file gallery.php */
 /* Location: ./app/controllers/gallery.php */

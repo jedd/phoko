1ad301d845f62ed1ec19681f028d30247efb6543
jedd <unclejedd@gmail.com>
2009-10-19 23:14:39 +0100

    Tweak - fix base URL, tweak CSS, add jquery.js

--- app/views/main_page.php
+++ app/views/main_page.php
@@ -1,13 +1,34 @@
 <?php
 	echo doctype('html4-trans');
-
-	echo "Here we are!";
-
-
-
 ?>
+<html>
+<head>
+<title>
+Phoko Album
+</title>
+
+<?php
+	echo "\n". link_tag('theme/default/gallery.css');
+
+	$jquery = array (
+				"href" => "js/jquery-1.3.2.js",
+				"type" => "text/javascript",
+				);
+	echo "\n". link_tag($jquery);
+?>
+
+</head>
+
+<body>
+
+Here we are
+
+
 
 
 <br />
 <hr />
 Page rendered in {elapsed_time} seconds
+
+</body>
+</html>

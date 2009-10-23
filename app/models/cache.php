e70c642dc904ae0cab0d1cc8634b347079f3682c
jedd <unclejedd@gmail.com>
2009-10-23 03:11:04 +0100

    Add - cache model

---
+++ app/models/cache.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Phoko
+ *
+ * Web gallery for a KPhotoAlbum repository
+ *
+ * @package		phoko
+ * @author		jedd
+ * @version		v1
+ * @copyright	Copyright (c) 2009, jedd
+ * @license		GPL 2 or later
+ * @link		http://dingogully.com.au/trac/phoko
+ **/
+
+// ------------------------------------------------------------------------
+
+/**
+ * Cache
+ *
+ * Back-end to the Cache management system
+ *
+ * Provides all primities for dealing with the cache - identifying
+ * present files, generating cache images when required, returning
+ * cached file information, and so on.
+ *
+ * @package     phoko
+ * @version     v1
+ * @author      jedd
+ * @link        http://dingogully.com.au/trac/phoko
+ **/
+
+// ------------------------------------------------------------------------
+
+class  Cache extends  Model {
+
+	// ------------------------------------------------------------------------
+	/**
+	 *   Attributes
+	 **/
+
+
+	// ------------------------------------------------------------------------
+	/**
+	 *   Constructor
+	 **/
+
+	function  __construct ()  {
+		parent::Model();
+		} // end-constructor
+
+
+	// ------------------------------------------------------------------------
+	/**
+	 * Something
+	 *
+	 *
+	 * @param	string		?
+	 * @return	?
+	 **/
+	function  something (  )  {
+
+
+
+		}  // end-method  something ()
+
+
+
+
+
+
+
+	// ========================================================================
+	// ------------------------------------------------------------------------
+	// P R I V A T E   F U N C T I O N S  -- nothing to see here.
+	// ------------------------------------------------------------------------
+	// ========================================================================
+
+	/**
+	 * @param
+	 * @return
+	 **/
+
+	}   // end-class  Cache ()

06b877d7665ea664999e2d28631ec38777a60c46
jedd <unclejedd@gmail.com>
2009-10-25 14:14:32 +0000

    Mod - session timeout set to 0 (no expiry)

--- app/config/config.php
+++ app/config/config.php
@@ -232,7 +232,7 @@ $config['encryption_key'] = "";
 |
 */
 $config['sess_cookie_name']		= 'phoko';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 0;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';

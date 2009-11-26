d858c0ec8185a7a9918326adcd06821a983e9626
jedd <unclejedd@gmail.com>
2009-11-26 00:55:10 +0000

    Tweak - tab choice stored in cookie (built-in to jquery) but not accordion status yet

--- js/phoko.js
+++ js/phoko.js
@@ -8,29 +8,11 @@
 // Tabs jQuery-UI effect
 $(function() {
 	$("#navi_tabs").tabs({
-		fx: { opacity: 'toggle' },
-		selected: 0,
-		select: function (event,ui) {
-			// var url = 'foobar' + $("#navi_tabs").attr("selectedIndex");
-			var url = ui.tab;
-			// var foo = url.attr(id);
-
-			// var tabs = $("#navi_tabs").tabs();
-			// var id = $('#selectBox').attr("selectedIndex");
-			// var selectedTab = tabs("option", "selected");
-
-			window.alert (url);
-
-
-//			$("#accordion").accordion('activate', 0);
-//			$("#accordion").accordion('activate', 1);
-//			$("#accordion").accordion('activate', 2);
-			}
+		fx:  {
+			opacity: 'toggle'
+			},
+		cookie: true,					// sadly there's no equiv in the accordion() world
 		});
-	//getter
-	// var cookie = $("#navi_tabs").tabs('option', 'cookie');
-	//setter
-	// $("#navi_tabs").tabs('option', 'cookie', { expires: 30 });
 	});
 
 // Accordion jQuery-UI effect
@@ -38,14 +20,11 @@ $(function() {
 	$("#accordion").accordion({
 		autoHeight: false,
 		header: 'h3',
-		collapsible: true,
-		navigation: true,
-		active: 0 // open first category at load by default
+		cookie: true,
+		collapsible: true
+		// navigation: true
+		// active: 0 // open first category at load by default
 		});
 	});
 
-$('#navi_tabs').each(function() {
-   var href = $.data(this, 'href.tabs');
-   console.log(href);
-})
 
